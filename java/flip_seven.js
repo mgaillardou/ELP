6a5ffa0efecaf904a1937ac8d633c3efe7d8df30
mgaillardou <marceau.gaillardou27@gmail.com>
2026-01-24 13:19:22 +0100

    projet js quasi fini plus que quelques bugs a corriger

--- java/flip_seven.js
+++ java/flip_seven.js
@@ -28,39 +28,98 @@ const dico_cartes = {
     "chance":3
 }
 
-let liste = [];
-
-for (const l in dico_cartes) 
-	{
-    const nb = dico_cartes[l];
-    const groupe = Array(nb).fill(l);
-    liste = [...liste, ...groupe];
+function melanger(paquet) {
+    for (let i = paquet.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [paquet[i], paquet[j]] = [paquet[j], paquet[i]];
+    }
 }
-const joueur1 = new Joueur("marceau");
+
+function genererPaquet() {
+    let nouveau = [];
+    for (const l in dico_cartes) {
+        const nb = dico_cartes[l];
+        nouveau = [...nouveau, ...Array(nb).fill(l)];
+    }
+    melanger(nouveau);
+    return nouveau;
+}
+
+let liste = genererPaquet()
+
+const joueur1 = new Joueur('marceau');
+const joueur2 = new Joueur('lilia');
+
+let liste_joueurs = [joueur1, joueur2]
+let joueurs_actifs = [...liste_joueurs]; // Copie des joueurs qui n'ont pas encore dit 'n' ou perdu
+let index_tour = 0;
+
 const prompt = promptSync();
 let fini = 0;
 
 
-while (fini === 0) {
-    console.log('tire une carte');
-    joueur1.jouerTour(liste)
+while (liste_joueurs.every(j => j.score < 200)) {
+    console.log("\n==========================");
+    console.log("   NOUVELLE MANCHE !");
+    console.log("==========================");
+    
+    let joueurs_actifs = [...liste_joueurs];
+    let index_tour = 0;
 
-    if (joueur1.cartes_main.length === 0){
-        break
-    }
-    else{
-        let choix = prompt("Veux tu continuer y/n ?");
-        if (choix === 'n'){
-            fini = 1
+	while (joueurs_actifs.length > 0) {
+
+		if (liste.length < 5) {
+            console.log("paquet vide");
+            liste = genererPaquet();
         }
-    }
+
+		let position = index_tour % joueurs_actifs.length;
+		let joueur_actuel = joueurs_actifs[position];
+
+		console.log(`\nC'est au tour de : ${joueur_actuel.nom.toUpperCase()}`);
+
+		console.log('cartes :' + joueur_actuel.cartes_main)
+		console.log('score temp '+ joueur_actuel.score_temp)
+
+		let choix = prompt("Veux tu continuer y/n ?");
+
+		if (choix === 'n'){
+				console.log(`${joueur_actuel.nom} se retire de la manche.`);
+
+				if (joueur_actuel.verif7Cartes().length >= 7){
+					joueur_actuel.score_temp += 15
+				}
+				if (joueur_actuel.cartes_main.includes('x2')){
+					joueur_actuel.score_temp = joueur_actuel.score_temp*2
+				}
+
+				joueur_actuel.etat = []
+				joueur_actuel.ajouteScore(); // Il valide ses points
+				joueurs_actifs.splice(position, 1);
+				joueur_actuel.cartes_main= []
+        		joueur_actuel.score_temp = 0
+        
+			}
+			else {
+				joueur_actuel.jouerTour(liste,liste_joueurs)
+				if (joueur_actuel.cartes_main.length === 0 || joueur_actuel.etat.includes("freeze")){
+					joueurs_actifs.splice(position, 1);
+					console.log(`💥 ${joueur_actuel.nom} a perdu et s'arrête pour cette manche.`);
+					joueur_actuel.etat = []
+					joueur_actuel.cartes_main= []
+        			joueur_actuel.score_temp = 0
+				}
+				else {
+					index_tour ++	
+				}
+			}
+		}
+	console.log('')
+	console.log('fin de manche')
+	liste_joueurs.forEach(j => console.log(`Score de ${j.nom} : ${j.score}`));
 }
-joueur1.ajouteScore()
-console.log(joueur1.score)
-console.log('')
-console.log('fin de tour')
+	
 
-
-
-
+const gagnant = liste_joueurs.reduce((prev, curr) => (prev.score > curr.score) ? prev : curr);
+console.log(`\n🏆 VICTOIRE DE ${gagnant.nom.toUpperCase()} avec ${gagnant.score} points ! 🏆`);
 
--- java/joueur.js
+++ java/joueur.js
@@ -1,3 +1,4 @@
+import promptSync from 'prompt-sync';
 export class Joueur {
 
     constructor(nom) {
@@ -5,11 +6,13 @@ export class Joueur {
         this.cartes_main= []
         this.score = 0
         this.score_temp = 0
+        this.etat = []
     }
 
     piocherCarte(paquet){
         const indice = Math.floor(Math.random()*paquet.length);
 	    const carte_prise = paquet.splice(indice,1)[0];
+        console.log('carte piochée : '+carte_prise)
 	    return carte_prise;
     } 
 	
@@ -22,12 +25,48 @@ export class Joueur {
         }
         return 1
     }
-        
+
+    verif7Cartes(){
+        const numerosPures = this.cartes_main.filter(carte => {
+        const estUnNombre = !isNaN(parseInt(carte));
+        const estUnBonus = carte.toString().includes('+') || carte.toString().includes('x');
+        return estUnNombre && !estUnBonus;
+        });
+        return numerosPures
+    }
+    recupereCarteSpeciale(carte, paquet, liste_joueurs){
+        if (carte === 'chance') {
+            this.cartes_main.unshift(carte); 
+            console.log('tu as une deuxième vie')
+        }
+        else if (carte === 'x2') {
+            this.cartes_main.push(carte); 
+            console.log('tu peux doubler le score de cette manche')
+        }
+        else{
+            const prompt = promptSync();
+            let choix = prompt('tu as cette carte spéciale : '+carte+' a qui veux tu la donner (1, 2, 3, 4')
+            liste_joueurs.forEach((joueur) => {
+                if (choix === joueur.nom)
+                    if (carte === 'freeze'){
+                        joueur.etat.push(carte)
+                    }
+                    else {
+                        for (let i=0; i<3; i++){
+                            joueur.jouerTour(paquet, liste_joueurs)
+                            if (joueur.cartes_main.length === 0){
+                                break
+                            }
+                        }
+                    }
+            })
+        }
+    }
     
-    jouerTour(paquet){
+    jouerTour(paquet, liste_joueurs){
         let carte = this.piocherCarte(paquet)
         let test = this.verifCarte(carte)
-        console.log('carte piochée : '+carte)
+        
         if (test === 1){
             this.cartes_main.push(carte);
             this.ajouteScoreTemp(carte)
@@ -35,12 +74,19 @@ export class Joueur {
             console.log('score temp '+ this.score_temp)
         }
         else if (test === 0){
-            console.log("Perdu");
-            this.cartes_main=[]
-            this.score_temp = 0
+            if (this.cartes_main[0] === 'chance'){
+                this.cartes_main.splice(0, 1); 
+            }
+            else {
+                console.log("Perdu");
+                this.cartes_main=[]
+                this.score_temp = 0
+            }
+            
         }
         else {
             console.log('carte speciale')
+            this.recupereCarteSpeciale(carte, paquet, liste_joueurs)
         }
     }
 
